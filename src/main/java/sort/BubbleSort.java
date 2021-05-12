89e8350345bdc94a57f719c2fac74700dbc1bdce
wangyanwen <2574071504@qq.com>
2021-05-12 22:45:04 +0800

    sort

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -3,6 +3,12 @@ package sort;
 import java.util.Arrays;
 
 /**
+ * 冒泡排序
+ *
+ * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+ * 针对所有的元素重复以上的步骤，除了最后一个。
+ * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
  *
  * @author wyw
  * @date 2021/05/12
@@ -11,11 +17,13 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] sourceArray = new int[]{34,4,2,7,6,18,20};
-        int[] targetArray = sort(sourceArray);
+
+        BubbleSort sorter = new BubbleSort();
+        int[] targetArray = sorter.sort(sourceArray);
         System.out.println(Arrays.toString(targetArray));
     }
 
-       public static int[] sort(int[] sourceArray) {
+       public int[] sort(int[] sourceArray) {
         // 对 arr 进行拷贝，不改变参数内容
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
---
+++ src/main/java/sort/SelectionSort.java
@@ -0,0 +1,50 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ *
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+ * 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 重复第二步，直到所有元素均排序完毕。
+ *
+ * @author wyw
+ * @date 2021/05/12
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] sourceArray = new int[]{34, 4, 2, 7, 6, 18, 20};
+
+        SelectionSort sorter = new SelectionSort();
+        int[] targetArray = sorter.sort(sourceArray);
+        System.out.println(Arrays.toString(targetArray));
+    }
+
+    public int[] sort(int[] sourceArray) {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        // 总共要经过 N-1 轮比较
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+
+            // 每轮需要比较的次数 N-i
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    // 记录目前能找到的最小值元素的下标
+                    min = j;
+                }
+            }
+
+            // 将找到的最小值和i位置所在的值进行交换
+            if (i != min) {
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+            }
+
+        }
+        return arr;
+    }
+}
